c129aeeb8b55a071dfdb46674c72d4c7a9624dd6
zhengguoqiang <zhengguoqiang>
2022-10-28 23:19:06 +0800

    go base

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -7,6 +7,17 @@ import (
 	"time"
 )
 
+//当标识符（包括常量、变量、类型、函数名、结构字段等等）以一个大写字母开头，如：Group1，那么使用这种形式的标识符的对象就可以被外部包的代码所使用（客户端程序需要先导入这个包），这被称为导出（像面向对象语言中的 public）；
+//标识符如果以小写字母开头，则对包外是不可见的，但是他们在整个包的内部是可见并且可用的（像面向对象语言中的 private ）
+/**
+	因此符合规范的函数一般写成如下的形式：
+	func functionName(parameter_list) (return_value_list) {
+   		…
+	}
+	parameter_list 的形式为 (param1 type1, param2 type2, …)
+	return_value_list 的形式为 (ret1 type1, ret2 type2, …)
+
+*/
 func Hello(name string) (string, error) {
 
 	if name == "" {
--- hello/hello.go
+++ hello/hello.go
@@ -8,7 +8,11 @@ import (
 	"example.com/greetings"
 )
 
-func main() {
+//go应用程序入口，在程序开始执行并完成初始化后，第一个调用（程序的入口点）的函数是 main.main()（如：C 语言），该函数一旦返回就表示程序已成功执行并立即退出。
+//该方法没有参数也没有返回值
+//左大括号 { 必须与方法的声明放在同一行，这是编译器的强制规定，否则你在使用 gofmt 时就会出现错误提示：`build-error: syntax error: unexpected semicolon or newline before {`
+//Go 语言虽然看起来不使用分号作为语句的结束，但实际上这一过程是由编译器自动完成，因此才会引发像上面这样的错误
+func test() {
 	// Set properties of the predefined Logger, including
 	// the log entry prefix and a flag to disable printing
 	// the time, source file, and line number.
